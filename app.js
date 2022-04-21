502c2762d3fbec3789dff322c0be4ef6f87a2ef2
Ian Murphy <murphy.ian24@gmail.com>
2022-04-21 15:42:38 -0300

    comienzo a hacer las categorias en el main incluyendo JS

---
+++ app.js
@@ -0,0 +1,5 @@
+//alert()
+movie__card.innerHTML  = `<div id="movie__card"><button class="header__btn">
+<div class="card__div"><img class="card__img" src="./assets/images/Document.jpg" width="250" alt=""></div>
+<h3 class="card__title">nombre</h3>
+</button></div>`
